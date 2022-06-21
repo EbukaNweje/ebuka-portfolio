d32abc12ea93fe2fc85ca93039a024b9dc44c95b
EbukaNweje <nwejeebuka@gmail.com>
2022-06-21 19:30:13 +0100

    Linter fix

--- Js/App.js
+++ Js/App.js
@@ -1,11 +1,11 @@
-Nav = document.querySelector('.items');
-HendMenu = document.querySelector('.bi-list');
-Close = document.querySelector('.close');
+const Nav = document.querySelector('.items');
+const HendMenu = document.querySelector('.bi-list');
+const Close = document.querySelector('.close');
 
-HendMenu.onclick = ()=> {
+HendMenu.onclick = () => {
     Nav.classList.add('show');
-}
+};
 
 Close.onclick = ()=> {
     Nav.classList.remove('show');
-}
+};
