404c1c8d98b4bbb528750169956cf1386da72c27
EbukaNweje <nwejeebuka@gmail.com>
2022-06-22 15:05:33 +0100

    Working on Required Changes

--- Js/App.js
+++ Js/App.js
@@ -1,11 +1,31 @@
 const Nav = document.querySelector('.items');
 const HendMenu = document.querySelector('.bi-list');
-const Close = document.querySelector('.close');
+const Close = document.querySelector('.close'); 
+const link = document.querySelector('.link'); 
+const link2 = document.querySelector('.link2'); 
+const link3 = document.querySelector('.link3'); 
 
 HendMenu.onclick = () => {
-    Nav.classList.add('show');
+  Nav.classList.add('show');
+  HendMenu.style.display = "none"
 };
 
 Close.onclick = ()=> {
-    Nav.classList.remove('show');
+  Nav.classList.remove('show');
+  HendMenu.style.display = "block"
+};
+
+link.onclick = ()=> {
+  Nav.classList.remove('show');
+  HendMenu.style.display = "block"
+};
+
+link2.onclick = ()=> {
+  Nav.classList.remove('show');
+  HendMenu.style.display = "block"
+};
+
+link3.onclick = ()=> {
+  Nav.classList.remove('show');
+  HendMenu.style.display = "block"
 };
